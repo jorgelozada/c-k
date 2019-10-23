6b8866bc7989a1290aed8c7aef9acd2df38c5299
mawaraujo <mauuroaraujo@gmail.com>
2019-10-23 18:00:36 -0300

    changes

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById('sideMenu').style.marginLeft = '0';
+    //document.body.style.backgroundColor = 'black';
+}
+
+function closeMenu() {
+    document.getElementById('sideMenu').style.marginLeft = '-100vw';
+    //document.body.style.backgroundColor = 'white';
+}
